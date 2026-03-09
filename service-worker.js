76623e5a37d95beb58e8d429c965e03c62c3a33b
cibevillamayorui-source <cibe.villamayor.ui@phinmaed.com>
2026-03-09 08:09:23 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,7 @@
-const CACHE_NAME = 'petspace-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/pets.json',
-  '/script.js',
-  '/manifest.json'
-];
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open("todo-cache").then(function(cache){
+      return cache.addAll(["index.html"])
     })
   );
 });
